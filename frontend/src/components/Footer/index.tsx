e6d7f65cc134181de48cebab80adb8ffedb90148
Luiz Henrique <luizhenrique.anjos10@hotmail.com>
2021-11-01 21:54:05 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/luiz-henrique-carvalho-dos-anjos-2750a61b8/" target="_blank" rel="noreferrer">Luiz Henrique</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
